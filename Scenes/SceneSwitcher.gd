49576dbbeebaec309399645587801f5ec78e0875
JoostPerdok <joostpperdok@gmail.com>
2022-06-01 11:53:16 +0200

    updates

--- Scenes/SceneSwitcher.gd
+++ Scenes/SceneSwitcher.gd
@@ -6,9 +6,7 @@ func _ready():
 	current_level.connect("level_changed", self, "handle_level_changed")
 	current_level.connect("ask_question", self, "handle_ask_question")
 
-
 func handle_level_changed(next_level_name: String):
-	
 	var next_level = load("res://Scenes/" + next_level_name + ".tscn").instance()
 	print(current_level, next_level, next_level_name)
 	add_child(next_level)
@@ -24,4 +22,3 @@ func handle_ask_question(next_level_name: String):
 	grumpy.connect("level_changed", self, "handle_level_changed")
 	grumpy.connect("ask_question", self, "handle_ask_question")
 	grumpy.next_level = next_level_name
-
